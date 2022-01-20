e5511163d23d43ebb642ca82420ed1e7229bebe5
Tyler Bantley <tyler.bantley@icloud.com>
2022-01-19 18:24:48 -0600

    tests approved, store node modules in gitignore, added requires to index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generateHTML = require('./src/page-template');
+const fs = require("fs"); 
+const team = [];
